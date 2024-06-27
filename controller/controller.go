65b72366be5eafc5cf134bd444e8ef5be19824ef
Daffa Audya Pramana <daffaaudyapramana03@gmail.com>
2024-06-28 00:41:45 +0700

    membuat controller POST menyimpan data

--- controller/controller.go
+++ controller/controller.go
@@ -1,5 +1,58 @@
 package controller
 
+// signup
+func SignUpPengguna(db *mongo.Database, insertedDoc model.Pengguna) error {
+	objectId := primitive.NewObjectID() 
+	if insertedDoc.NamaLengkap == "" || insertedDoc.TanggalLahir == "" || insertedDoc.JenisKelamin == "" || insertedDoc.NomorHP == "" || insertedDoc.Alamat == "" || insertedDoc.Akun.Email == "" || insertedDoc.Akun.Password == "" {
+		return fmt.Errorf("Dimohon untuk melengkapi data")
+	} 
+	if err := checkmail.ValidateFormat(insertedDoc.Akun.Email); err != nil {
+		return fmt.Errorf("Email tidak valid")
+	} 
+	userExists, _ := GetUserFromEmail(insertedDoc.Akun.Email, db)
+	if insertedDoc.Akun.Email == userExists.Email {
+		return fmt.Errorf("Email sudah terdaftar")
+	} 
+	if strings.Contains(insertedDoc.Akun.Password, " ") {
+		return fmt.Errorf("password tidak boleh mengandung spasi")
+	}
+	if len(insertedDoc.Akun.Password) < 8 {
+		return fmt.Errorf("password terlalu pendek")
+	} 
+	salt := make([]byte, 16)
+	_, err := rand.Read(salt)
+	if err != nil {
+		return fmt.Errorf("kesalahan server : salt")
+	}
+	hashedPassword := argon2.IDKey([]byte(insertedDoc.Akun.Password), salt, 1, 64*1024, 4, 32)
+	user := bson.M{
+		"_id": objectId,
+		"email": insertedDoc.Akun.Email,
+		"password": hex.EncodeToString(hashedPassword),
+		"salt": hex.EncodeToString(salt),
+		"role": "pengguna",
+	}
+	pengguna := bson.M{
+		"namalengkap": insertedDoc.NamaLengkap,
+		"tanggallahir": insertedDoc.TanggalLahir,
+		"jeniskelamin": insertedDoc.JenisKelamin,
+		"nomorhp": insertedDoc.NomorHP,
+		"alamat": insertedDoc.Alamat,
+		"akun": model.User {
+			ID : objectId,
+		},
+	}
+	_, err = InsertOneDoc(db, "user", user)
+	if err != nil {
+		return fmt.Errorf("kesalahan server")
+	}
+	_, err = InsertOneDoc(db, "pengguna", pengguna)
+	if err != nil {
+		return fmt.Errorf("kesalahan server")
+	}
+	return nil
+}
+
 //user
 func GetAllUser(db *mongo.Database) (user []model.User, err error) {
 	collection := db.Collection("user")
