163d273bdc28ab638f4db08cf792d5440bb5cbe0
Daffa Audya Pramana <daffaaudyapramana03@gmail.com>
2024-06-28 00:12:07 +0700

    backend

--- controller/controller.go
+++ controller/controller.go
@@ -1,1 +1,16 @@
 package controller
+
+func GetAllUser(db *mongo.Database) (user []model.User, err error) {
+	collection := db.Collection("user")
+	filter := bson.M{}
+	cursor, err := collection.Find(context.Background(), filter)
+	if err != nil {
+		return user, fmt.Errorf("error GetAllUser mongo: %s", err)
+	}
+	err = cursor.All(context.Background(), &user)
+	if err != nil {
+		return user, fmt.Errorf("error GetAllUser context: %s", err)
+	}
+	return user, nil
+}
+
