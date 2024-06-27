3df592f5b0a21b5513fcfd699c6d4e4a27840740
Daffa Audya Pramana <daffaaudyapramana03@gmail.com>
2024-06-28 00:19:27 +0700

    membuat controller login

--- controller/controller.go
+++ controller/controller.go
@@ -1,5 +1,6 @@
 package controller
 
+//user
 func GetAllUser(db *mongo.Database) (user []model.User, err error) {
 	collection := db.Collection("user")
 	filter := bson.M{}
@@ -14,3 +15,25 @@ func GetAllUser(db *mongo.Database) (user []model.User, err error) {
 	return user, nil
 }
 
+//login
+func LogIn(db *mongo.Database, insertedDoc model.User) (user model.User, err error) {
+	if insertedDoc.Email == "" || insertedDoc.Password == "" {
+		return user, fmt.Errorf("Dimohon untuk melengkapi data")
+	} 
+	if err = checkmail.ValidateFormat(insertedDoc.Email); err != nil {
+		return user, fmt.Errorf("Email tidak valid")
+	} 
+	existsDoc, err := GetUserFromEmail(insertedDoc.Email, db)
+	if err != nil {
+		return 
+	}
+	salt, err := hex.DecodeString(existsDoc.Salt)
+	if err != nil {
+		return user, fmt.Errorf("kesalahan server : salt")
+	}
+	hash := argon2.IDKey([]byte(insertedDoc.Password), salt, 1, 64*1024, 4, 32)
+	if hex.EncodeToString(hash) != existsDoc.Password {
+		return user, fmt.Errorf("password salah")
+	}
+	return existsDoc, nil
+}
