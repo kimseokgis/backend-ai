796f82ac2a79c9d1bc67e2de76ad38fc039c3479
rofinafiin <rofinafiisr@gmail.com>
2024-07-03 21:33:16 +0700

    Membuat struct Chats

--- model/model.go
+++ model/model.go
@@ -5,3 +5,9 @@ type User struct {
 	Username string `json:"username" bson:"username"`
 	Password string `json:"password" bson:"password"`
 }
+
+type Chats struct {
+	IdChats   string `json:"id_chats" bson:"idChats"`
+	Message   string `json:"message" bson:"message"`
+	Responses string `json:"responses" bson:"responses"`
+}
