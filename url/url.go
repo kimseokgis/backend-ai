2d4964c6c89a6264aed75cd718124d5779f4ac7d
rofinafiin <rofinafiisr@gmail.com>
2024-07-06 23:44:43 +0700

    try switch case without validate the token

--- url/url.go
+++ url/url.go
@@ -1,8 +1,20 @@
 package url
 
 import (
+	"github.com/kimseokgis/backend-ai/config"
+	"github.com/kimseokgis/backend-ai/controller"
 	"net/http"
 )
 
-func Web(http.ResponseWriter, *http.Request) {
+func Web(w http.ResponseWriter, r *http.Request) {
+	if config.SetAccessControlHeaders(w, r) {
+		return // If it's a preflight request, return early.
+	}
+	var method, path string = r.Method, r.URL.Path
+	switch {
+	case method == "GET" && path == "/":
+		controller.HomeMakmur(w, r)
+	default:
+		controller.NotFound(w, r)
+	}
 }
