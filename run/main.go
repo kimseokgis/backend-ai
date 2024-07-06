ecce222daab34dea2ff33a70f44d2f9434ffe05d
rofinafiin <rofinafiisr@gmail.com>
2024-07-06 21:35:41 +0700

    move to folder run

--- run/main.go
+++ run/main.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/log"
 	"github.com/gofiber/fiber/v2/middleware/cors"
-	"github.com/kimseokgis/backen-ai/config"
-	"github.com/kimseokgis/backen-ai/url"
 )
 
 func main() {
