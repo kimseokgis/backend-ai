9fd1433e0fb482ddd5c6eb43615547f36aeaa15f
rofinafiin <rofinafiisr@gmail.com>
2024-07-06 23:45:12 +0700

    handling cors for gcp

--- config/cors.go
+++ config/cors.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"net/http"
 	"os"
 	"strings"
 )
@@ -30,4 +31,36 @@ var Cors = cors.Config{
 	AllowHeaders:     "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Request-Headers, token, Access-Control-Allow-Origin, Authorization, Bearer",
 	ExposeHeaders:    "Content-Length",
 	AllowCredentials: true,
+}
+
+func isAllowedOrigin(origin string) bool {
+	for _, o := range origins {
+		if o == origin {
+			return true
+		}
+	}
+	return false
+}
+
+func SetAccessControlHeaders(w http.ResponseWriter, r *http.Request) bool {
+	origin := r.Header.Get("Origin")
+
+	if isAllowedOrigin(origin) {
+		// Set CORS headers for the preflight request
+		if r.Method == http.MethodOptions {
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Login")
+			w.Header().Set("Access-Control-Allow-Methods", "POST,GET,DELETE,PUT")
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Max-Age", "3600")
+			w.WriteHeader(http.StatusNoContent)
+			return true
+		}
+		// Set CORS headers for the main request.
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Origin", origin)
+		return false
+	}
+
+	return false
 }
