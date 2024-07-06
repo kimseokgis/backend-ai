2b83b5483ea398a1346d301759c4050e7f182eb4
rofinafiin <rofinafiisr@gmail.com>
2024-07-06 23:45:26 +0700

    new functions for write responses

---
+++ helper/responses.go
@@ -0,0 +1,21 @@
+package helper
+
+import (
+	"encoding/json"
+	"github.com/gofiber/fiber/v2/log"
+	"net/http"
+)
+
+func WriteJSON(respw http.ResponseWriter, statusCode int, content interface{}) {
+	respw.Header().Set("Content-Type", "application/json")
+	respw.WriteHeader(statusCode)
+	respw.Write([]byte(Jsonstr(content)))
+}
+
+func Jsonstr(strc interface{}) string {
+	jsonData, err := json.Marshal(strc)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(jsonData)
+}
