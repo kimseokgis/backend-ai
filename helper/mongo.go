f47d8c091ed72863fa71b44976c6c2e4fc227df8
rofinafiin <rofinafiisr@gmail.com>
2024-07-06 23:51:47 +0700

    moving out all the functions cycle

--- helper/mongo.go
+++ helper/mongo.go
@@ -2,9 +2,13 @@ package helper
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
-	"github.com/kimseokgis/backend-ai/controller"
 	"github.com/kimseokgis/backend-ai/model"
+	"github.com/whatsauth/watoken"
+	"go.mongodb.org/mongo-driver/bson"
+	"golang.org/x/crypto/bcrypt"
+	"net/http"
 	"os"
 
 	"github.com/aiteung/atdb"
@@ -48,7 +52,7 @@ func InsertOneDoc(db *mongo.Database, collection string, doc interface{}) (inser
 
 func CreateNewUserRole(mongoconn *mongo.Database, collection string, userdata model.User) interface{} {
 	// Hash the password before storing it
-	hashedPassword, err := controller.HashPass(userdata.PasswordHash)
+	hashedPassword, err := HashPass(userdata.PasswordHash)
 	if err != nil {
 		return err
 	}
@@ -56,4 +60,92 @@ func CreateNewUserRole(mongoconn *mongo.Database, collection string, userdata mo
 
 	// Insert the admin data into the database
 	return atdb.InsertOneDoc(mongoconn, collection, userdata)
+}
+
+// func Login Register
+func Register(Mongoenv, dbname string, r *http.Request) string {
+	resp := new(model.Credential)
+	userdata := new(model.User)
+	resp.Status = false
+	conn := MongoCreateConnection(Mongoenv, dbname)
+	err := json.NewDecoder(r.Body).Decode(userdata)
+	if err != nil {
+		resp.Message = "error parsing application/json: " + err.Error()
+	} else {
+		resp.Status = true
+		hash, err := HashPass(userdata.PasswordHash)
+		if err != nil {
+			resp.Message = "Gagal Hash Password" + err.Error()
+		}
+		InsertUserdata(conn, userdata.Username, userdata.Email, userdata.Password, hash)
+		resp.Message = "Berhasil Registrasi Data"
+	}
+	response := ReturnStringStruct(resp)
+	return response
+}
+
+// func Login User
+func Login(PASETOPRIVATEKEYENV, MONGOCONNSTRINGENV, dbname, collectionname string, r *http.Request) string {
+	var Response model.Credential
+	Response.Status = false
+	mconn := SetConnection(dbname)
+	var datauser model.User
+	err := json.NewDecoder(r.Body).Decode(&datauser)
+	if err != nil {
+		Response.Message = "error parsing application/json: " + err.Error()
+	} else {
+		if IsPasswordValid(mconn, collectionname, datauser) {
+			Response.Status = true
+			tokenstring, err := watoken.Encode(datauser.Username, os.Getenv(PASETOPRIVATEKEYENV))
+			if err != nil {
+				Response.Message = "Gagal Encode Token : " + err.Error()
+			} else {
+				Response.Message = "Selamat Datang Anda Berhasil Login"
+				Response.Token = tokenstring
+			}
+		} else {
+			Response.Message = "Username atau Password Anda Salah"
+		}
+	}
+
+	return GCFReturnStruct(Response)
+}
+
+// return struct
+func GCFReturnStruct(DataStuct any) string {
+	jsondata, _ := json.Marshal(DataStuct)
+	return string(jsondata)
+}
+
+func ReturnStringStruct(Data any) string {
+	jsonee, _ := json.Marshal(Data)
+	return string(jsonee)
+}
+
+// Password
+func HashPass(passwordhash string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(passwordhash), 14)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(passwordhash, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(passwordhash))
+	return err == nil
+}
+
+func IsPasswordValid(mongoconn *mongo.Database, collection string, userdata model.User) bool {
+	filter := bson.M{
+		"$or": []bson.M{
+			{"username": userdata.Username},
+			{"email": userdata.Email},
+		},
+	}
+
+	var res model.User
+	err := mongoconn.Collection(collection).FindOne(context.TODO(), filter).Decode(&res)
+
+	if err == nil {
+		return CheckPasswordHash(userdata.PasswordHash, res.PasswordHash)
+	}
+	return false
 }
