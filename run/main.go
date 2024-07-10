1cd9619872fd768f634a98a370cfff54c2414bf1
Farhan Rizki <farhanrizki101010@gmail.com>
2024-07-10 23:47:13 +0700

    update controller users get all user

--- run/main.go
+++ run/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
-	"github.com/kimseokgis/backend-ai/url"
 	"net/http"
+
+	"github.com/kimseokgis/backend-ai/controller"
+	"github.com/kimseokgis/backend-ai/url"
 )
 
 func main() {
 	http.HandleFunc("/", url.Web)
+
+	http.HandleFunc("/getuser", controller.GetUser)
+	
 	http.ListenAndServe(":8080", nil)
 }
