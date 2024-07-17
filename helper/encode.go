2d68a91b8a320a99a622fad6ee53998af18ea51e
rofinafiin <rofinafiisr@gmail.com>
2024-07-17 23:34:49 +0700

    function encode token with username

--- helper/encode.go
+++ helper/encode.go
@@ -3,6 +3,7 @@ package helper
 import (
 	"encoding/json"
 	"fmt"
+	"time"
 
 	"aidanwoods.dev/go-paseto"
 	"github.com/kimseokgis/backend-ai/model"
@@ -23,4 +24,14 @@ func Decoder(publickey, tokenstr string) (payload model.Payload, err error) {
 		json.Unmarshal(token.ClaimsJSON(), &payload)
 	}
 	return payload, err
+}
+
+func EncodeWithUsername(username, privatekey string) (string, error) {
+	token := paseto.NewToken()
+	token.SetIssuedAt(time.Now())
+	token.SetNotBefore(time.Now())
+	token.SetExpiration(time.Now().Add(2 * time.Hour))
+	token.SetString("user", username)
+	key, err := paseto.NewV4AsymmetricSecretKeyFromHex(privatekey)
+	return token.V4Sign(key, nil), err
 }
