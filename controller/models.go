68012c743a5659d40c0181bfd3f7a497feba3fb6
rofinafiin <rofinafiisr@gmail.com>
2024-07-17 23:13:16 +0700

    state return

--- controller/models.go
+++ controller/models.go
@@ -19,17 +19,20 @@ func ChatPredictUsingRegexp(w http.ResponseWriter, r *http.Request) {
 		resp.Message = "token is empty"
 		resp.Status = false
 		helper.WriteJSON(w, http.StatusNotAcceptable, resp)
+		return
 	}
-	err := json.NewDecoder(r.Body).Decode(req)
+	err := json.NewDecoder(r.Body).Decode(&req)
 	if err != nil {
 		resp.Message = "error parsing application/json: " + err.Error()
 		helper.WriteJSON(w, http.StatusNotAcceptable, resp)
+		return
 	} else {
 		decoder, err := helper.Decoder(config.PublicKey, token)
 		if err != nil {
 			resp.Message = err.Error()
 			resp.Status = false
 			helper.WriteJSON(w, http.StatusBadRequest, resp)
+			return
 		}
 		db := helper.SetConnection()
 		_, err = helper.FindUserByUsername(db, decoder.User)
@@ -37,6 +40,7 @@ func ChatPredictUsingRegexp(w http.ResponseWriter, r *http.Request) {
 			resp.Message = fmt.Sprintf("Data tidak ditemukan : %s\n", err.Error())
 			resp.Status = false
 			helper.WriteJSON(w, http.StatusNotFound, resp)
+			return
 		}
 
 		reply, err := helper.QueriesDataRegexp(db, context.TODO(), req.Messages)
@@ -44,11 +48,12 @@ func ChatPredictUsingRegexp(w http.ResponseWriter, r *http.Request) {
 			resp.Message = fmt.Sprintf("error get Replies: %s\n", err.Error())
 			resp.Status = false
 			helper.WriteJSON(w, http.StatusNotFound, resp)
+			return
 		}
 		chat.IdChats = reply.ID.Hex()
 		chat.Message = reply.Question
 		chat.Responses = reply.Answer
 	}
 	helper.WriteJSON(w, http.StatusOK, chat)
-
+	return
 }
