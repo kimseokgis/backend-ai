6086ccbbc627f2a2b0ad6da6cf811e3152135f44
rofinafiin <rofinafiisr@gmail.com>
2024-07-18 09:03:16 +0700

    add login allow headers

--- config/cors.go
+++ config/cors.go
@@ -51,7 +51,7 @@ func SetAccessControlHeaders(w http.ResponseWriter, r *http.Request) bool {
 		// Set CORS headers for the preflight request
 		if r.Method == http.MethodOptions {
 			w.Header().Set("Access-Control-Allow-Credentials", "true")
-			w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Request-Headers, token, Access-Control-Allow-Origin, Authorization, Bearer")
+			w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Request-Headers, token, Access-Control-Allow-Origin, Authorization, Bearer, login")
 			w.Header().Set("Access-Control-Allow-Methods", "POST,GET,DELETE,PUT")
 			w.Header().Set("Access-Control-Allow-Origin", origin)
 			w.Header().Set("Access-Control-Max-Age", "3600")
