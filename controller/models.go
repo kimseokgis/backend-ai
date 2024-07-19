b07283e138b0df94d3e7c36c200719d368145962
rofinafiin <rofinafiisr@gmail.com>
2024-07-19 17:42:07 +0700

    set reply answer jika kosong

--- controller/models.go
+++ controller/models.go
@@ -71,6 +71,12 @@ func ChatPredictForDomykado(w http.ResponseWriter, r *http.Request) {
 	resp := new(model.Credential)
 	chat := new(model.Chats)
 	mess := new(model.Requests)
+	err := json.NewDecoder(r.Body).Decode(&mess)
+	if err != nil {
+		resp.Message = "error parsing application/json: " + err.Error()
+		helper.WriteJSON(w, http.StatusNotAcceptable, resp)
+		return
+	}
 	token := r.Header.Get("secret")
 	if token == "" {
 		resp.Message = "secret is empty"
@@ -78,36 +84,36 @@ func ChatPredictForDomykado(w http.ResponseWriter, r *http.Request) {
 		helper.WriteJSON(w, http.StatusNotAcceptable, resp)
 		return
 	}
-	err := json.NewDecoder(r.Body).Decode(mess)
-	if err != nil {
-		resp.Message = "error parsing application/json: " + err.Error()
-		helper.WriteJSON(w, http.StatusNotAcceptable, resp)
-	} else {
-		db := helper.SetConnection()
-		//fmt.Println(decoder)
+	fmt.Println(mess)
+	db := helper.SetConnection()
+	//fmt.Println(decoder)
 
-		_, err = helper.QueriesSecret(db, context.TODO(), token)
-		if err != nil {
-			resp.Message = fmt.Sprintf("Data tidak ditemukan : %s\n", err.Error())
-			resp.Status = false
-			helper.WriteJSON(w, http.StatusNotFound, resp)
-			return
-		}
-		fmt.Printf("%+v\n", mess.Messages)
-		reply, score, err := helper.QueriesDataRegexpALL(db, context.TODO(), mess.Messages)
-		if err != nil {
-			resp.Message = "Aduh aduh aduhhhaiii, aku ga ngerti nihh coba nanya yang lain dongg biar aku ngertiin kamu..."
-			resp.Status = false
-			chat.Responses = resp.Message
-			helper.WriteJSON(w, http.StatusNotFound, resp)
-			return
-		}
-		chat.IdChats = reply.ID.Hex()
-		chat.Message = reply.Question
-		chat.Responses = reply.Answer
-		chat.Score = score
-		defer db.Client().Disconnect(context.Background())
+	_, err = helper.QueriesSecret(db, context.TODO(), token)
+	if err != nil {
+		resp.Message = fmt.Sprintf("Data tidak ditemukan : %s\n", err.Error())
+		resp.Status = false
+		helper.WriteJSON(w, http.StatusNotFound, resp)
+		return
 	}
+	fmt.Printf("%+v\n", mess.Messages)
+	reply, score, err := helper.QueriesDataRegexpALL(db, context.TODO(), mess.Messages)
+	if err != nil {
+		resp.Message = "Aduh aduh aduhhhaiii, aku ga ngerti nihh coba nanya yang lain dongg biar aku ngertiin kamu..."
+		resp.Status = false
+		chat.Responses = resp.Message
+		helper.WriteJSON(w, http.StatusNotFound, resp)
+		return
+	}
+
+	chat.IdChats = reply.ID.Hex()
+	chat.Message = reply.Question
+	chat.Responses = reply.Answer
+	chat.Score = score
+	if reply.Answer == "" {
+		chat.Message = "Adududududu aku ga ngerti nih kakak"
+	}
+	defer db.Client().Disconnect(context.Background())
+
 	helper.WriteJSON(w, http.StatusOK, chat)
 	return
 }
