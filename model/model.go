c7b49523ee7416a65ad03df1bbfa7242096dcc94
rofinafiin <rofinafiisr@gmail.com>
2024-07-19 16:07:06 +0700

    add score to model

--- model/model.go
+++ model/model.go
@@ -5,9 +5,10 @@ import (
 )
 
 type Chats struct {
-	IdChats   string `json:"id_chats" bson:"idChats"`
-	Message   string `json:"message" bson:"message"`
-	Responses string `json:"responses" bson:"responses"`
+	IdChats   string  `json:"id_chats" bson:"idChats"`
+	Message   string  `json:"message" bson:"message"`
+	Responses string  `json:"responses" bson:"responses"`
+	Score     float64 `json:"score" bson:"score"`
 }
 
 type User struct {
