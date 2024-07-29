ec5108c1c89f96698cecaaa9da93feb33e70d363
harisriyoni3 <1214047@std.ulbi.ac.id>
2024-07-30 06:40:05 +0700

    update

--- local/controllers/user_contoller.go
+++ local/controllers/user_contoller.go
@@ -1,7 +1,16 @@
 package controllers
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/kimseokgis/backend-ai/model"
+)
 
 func RegisterUser(w http.ResponseWriter, r *http.Request) {
-
+	var user model.User
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
 }
