cca28e54f52980d71a7443669700df6a45eb0cb1
harisriyoni3 <1214047@std.ulbi.ac.id>
2024-07-30 06:37:15 +0700

    update function

--- local/controllers/local_controller.go
+++ local/controllers/local_controller.go
@@ -2,8 +2,13 @@ package controllers
 
 import (
 	"net/http"
+
+	"github.com/kimseokgis/backend-ai/helper"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
-
+	response := map[string]string{
+		"message": "Welcome to Makmur AI!",
+	}
+	helper.WriteJSON(w, http.StatusOK, response)
 }
