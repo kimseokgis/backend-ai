1dcfa77159bff2ec3125c64a97cfd7c55d8e4fda
harisriyoni3 <1214047@std.ulbi.ac.id>
2024-07-30 06:40:32 +0700

    set connection

--- local/controllers/user_contoller.go
+++ local/controllers/user_contoller.go
@@ -1,9 +1,11 @@
 package controllers
 
 import (
+	"context"
 	"encoding/json"
 	"net/http"
 
+	"github.com/kimseokgis/backend-ai/helper"
 	"github.com/kimseokgis/backend-ai/model"
 )
 
@@ -12,5 +14,7 @@ func RegisterUser(w http.ResponseWriter, r *http.Request) {
 	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
 		http.Error(w, "Invalid request payload", http.StatusBadRequest)
 		return
+		conn := helper.SetConnection()
+		defer conn.Client().Disconnect(context.TODO())
 	}
 }
