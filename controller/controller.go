af6958ece9db156f413e029551a1695a27897304
rofinafiin <rofinafiisr@gmail.com>
2024-07-29 10:30:26 +0700

    fix returning inserted count

--- controller/controller.go
+++ controller/controller.go
@@ -29,12 +29,15 @@ func Comment(respw http.ResponseWriter, req *http.Request) {
 	var resp model.Response
 	comment := new(model.Comment)
 	resp.Status = false
-	_ = helper.SetConnection()
+	conn := helper.SetConnection()
 	err := json.NewDecoder(req.Body).Decode(comment)
 	if err != nil {
 		resp.Message = "error parsing application/json: " + err.Error()
 		helper.WriteJSON(respw, http.StatusNotAcceptable, resp)
 	}
-
+	insID := helper.InsertComment(conn, *comment)
+	resp.Status = true
+	resp.Message = fmt.Sprintf("Data berhasil diinsert %s", insID)
+	helper.WriteJSON(respw, http.StatusOK, resp)
 	return
 }
