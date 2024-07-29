4a9a800bf3cb5eea01574ed1772b2e93f6db15e8
harisriyoni3 <1214047@std.ulbi.ac.id>
2024-07-30 06:21:53 +0700

    update struct

--- local/models/model.go
+++ local/models/model.go
@@ -3,7 +3,9 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
-	ID       primitive.ObjectID `bson:"_id,omitempty"`
-	Username string             `json:"username"`
-	Email    string             `json:"email"`
+	ID           primitive.ObjectID `bson:"_id,omitempty"`
+	Username     string             `json:"username"`
+	Email        string             `json:"email"`
+	Password     string             `json:"password"`
+	PasswordHash string             `json:"password_hash"`
 }
