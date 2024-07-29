4429f6a9da04f9e932d60da5e1711c6f660fdfc0
harisriyoni3 <1214047@std.ulbi.ac.id>
2024-07-30 06:44:45 +0700

    set db mongo

--- local/helpers/db.go
+++ local/helpers/db.go
@@ -1,7 +1,16 @@
 package helpers
 
-import "go.mongodb.org/mongo-driver/mongo"
+import (
+	"os"
+
+	"github.com/aiteung/atdb"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 func SetConnection() *mongo.Database {
-
+	var DBmongoinfo = atdb.DBInfo{
+		DBString: os.Getenv("MONGOSTRING"),
+		DBName:   "AI",
+	}
+	return atdb.MongoConnect(DBmongoinfo)
 }
