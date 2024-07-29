7b7a8389c47bb383ce7a44f90751dead29caf892
harisriyoni3 <1214047@std.ulbi.ac.id>
2024-07-30 06:50:28 +0700

    set expired/expiration

--- local/helpers/auth.go
+++ local/helpers/auth.go
@@ -10,4 +10,5 @@ func GenerateToken(username, privatekey string) (string, error) {
 	token := paseto.NewToken()
 	token.SetIssuedAt(time.Now())
 	token.SetNotBefore(time.Now())
+	token.SetExpiration(time.Now().Add(2 * time.Hour))
 }
