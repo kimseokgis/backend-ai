aa059b572142c2b4ab0ac740b6a5773c1ffd596c
harisriyoni3 <1214047@std.ulbi.ac.id>
2024-07-30 06:25:45 +0700

    update prefork ,case sensitive, strict routing

--- local/config/config.go
+++ local/config/config.go
@@ -2,4 +2,8 @@ package config
 
 import "github.com/gofiber/fiber/v2"
 
-var FiberConfig = fiber.Config{}
+var FiberConfig = fiber.Config{
+	Prefork:       true,
+	CaseSensitive: true,
+	StrictRouting: true,
+}
