75924ddc7f5d05322e6e9e42c0cdba239d46db8d
harisriyoni3 <1214047@std.ulbi.ac.id>
2024-07-30 06:50:47 +0700

    update

--- local/helpers/auth.go
+++ local/helpers/auth.go
@@ -11,4 +11,10 @@ func GenerateToken(username, privatekey string) (string, error) {
 	token.SetIssuedAt(time.Now())
 	token.SetNotBefore(time.Now())
 	token.SetExpiration(time.Now().Add(2 * time.Hour))
+	token.SetString("user", username)
+	key, err := paseto.NewV4AsymmetricSecretKeyFromHex(privatekey)
+	if err != nil {
+		return "", err
+	}
+	return token.V4Sign(key, nil), nil
 }
