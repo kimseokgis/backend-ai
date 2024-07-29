f07024e36132ebe0a9d3ee1765a11e1130a30b20
harisriyoni3 <1214047@std.ulbi.ac.id>
2024-07-30 06:50:01 +0700

    set paseto token

--- local/helpers/auth.go
+++ local/helpers/auth.go
@@ -1,1 +1,13 @@
 package helpers
+
+import (
+	"time"
+
+	"aidanwoods.dev/go-paseto"
+)
+
+func GenerateToken(username, privatekey string) (string, error) {
+	token := paseto.NewToken()
+	token.SetIssuedAt(time.Now())
+	token.SetNotBefore(time.Now())
+}
