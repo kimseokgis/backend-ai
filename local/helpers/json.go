ba49ec85f5ae9c7fda53850d3b2b9559fa295c80
Farhan Rizki <farhanrizki101010@gmail.com>
2024-07-31 04:24:34 +0700

    add file

--- local/helpers/json.go
+++ local/helpers/json.go
@@ -5,3 +5,8 @@ import (
 	"net/http"
 )
 
+func WriteJSON(w http.ResponseWriter, status int, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(data)
+}
