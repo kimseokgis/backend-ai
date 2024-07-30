3f0f94d094ca622a46a978eca990c29ec23ba7b3
Farhan Rizki <farhanrizki101010@gmail.com>
2024-07-31 04:03:39 +0700

    delete func main

--- local/main.go
+++ local/main.go
@@ -10,25 +10,4 @@ import (
 	"github.com/kimseokgis/backend-ai/local/controllers"
 )
 
-func main() {
-	app := fiber.New(config.FiberConfig)
 
-	// Middleware
-	app.Use(logger.New())
-
-	// Routes
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Welcome to Makmur AI!")
-	})
-	app.Post("/register", controllers.RegisterUser)
-	app.Post("/login", controllers.LoginUser)
-
-	// Determine port
-	port := os.Getenv("PORT") // Try to get PORT from environment variable
-	if port == "" {
-		port = "8080" // Default port
-	}
-
-	log.Printf("Server is running on http://127.0.0.1:%s\n", port)
-	log.Fatal(app.Listen(":" + port))
-}
