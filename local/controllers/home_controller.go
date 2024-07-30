8f3ccbffa2b7a718d9d181cd498581683cc689c6
Farhan Rizki <farhanrizki101010@gmail.com>
2024-07-31 03:46:57 +0700

    add create home response

--- local/controllers/home_controller.go
+++ local/controllers/home_controller.go
@@ -6,4 +6,11 @@ import (
 	"github.com/kimseokgis/backend-ai/helper"
 )
 
+// createHomeResponse creates the response for the Home handler.
+// Returns a map with a welcome message.
+func createHomeResponse() map[string]string {
+	return map[string]string{
+		"message": "Welcome to Makmur AI!",
+	}
+}
 
