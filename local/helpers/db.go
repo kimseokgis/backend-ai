e14d495536fa47587db4eab01492215cea626596
harisriyoni3 <1214047@std.ulbi.ac.id>
2024-07-30 07:14:20 +0700

    func check password hash

--- local/helpers/db.go
+++ local/helpers/db.go
@@ -43,3 +43,8 @@ func HashPass(passwordhash string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(passwordhash), 14)
 	return string(bytes), err
 }
+
+func CheckPasswordHash(passwordhash, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(passwordhash))
+	return err == nil
+}
