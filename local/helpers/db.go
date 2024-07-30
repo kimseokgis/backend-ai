1479c9a0484a5cad3b428df37b09532649d25ab8
Farhan Rizki <farhanrizki101010@gmail.com>
2024-07-31 04:22:25 +0700

    delete comment

--- local/helpers/db.go
+++ local/helpers/db.go
@@ -11,34 +11,3 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-// getDBInfo constructs the database info from environment variables.
-
-
-// connectToMongoDB establishes a connection to MongoDB.
-
-
-// insertUserToDB inserts a user into the specified MongoDB collection.
-
-
-// findUserInDB finds a user in the specified MongoDB collection by username.
-
-
-// hashPassword generates a hashed password using bcrypt.
-
-
-// checkPasswordHash compares a hashed password with a plain password.
-
-
-// SetConnection establishes a connection to the MongoDB database.
-
-
-// InsertUser inserts a user into the "users" collection in the database.
-
-
-// ValidatePassword validates the user's password against the stored hash in the database.
-
-
-// HashPass hashes a password using bcrypt.
-
-
-// CheckPasswordHash compares a hashed password with a plain password.
