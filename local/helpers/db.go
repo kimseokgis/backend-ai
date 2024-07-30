bb7ec5d4a574921868ce31f98cb50dc574fff7fc
Farhan Rizki <farhanrizki101010@gmail.com>
2024-07-31 04:23:56 +0700

    add func in db go

--- local/helpers/db.go
+++ local/helpers/db.go
@@ -11,3 +11,42 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+func SetConnection() *mongo.Database {
+	var DBmongoinfo = atdb.DBInfo{
+		DBString: os.Getenv("MONGOSTRING"),
+		DBName:   "AI",
+	}
+	return atdb.MongoConnect(DBmongoinfo)
+}
+
+func InsertUser(db *mongo.Database, user model.User) interface{} {
+	collection := db.Collection("users")
+	result, err := collection.InsertOne(context.TODO(), user)
+	if err != nil {
+		return nil
+	}
+	return result.InsertedID
+}
+
+func ValidatePassword(conn *mongo.Database, user model.User) bool {
+	collection := conn.Collection("users")
+	filter := bson.M{
+		"username": user.Username,
+	}
+	var storedUser model.User
+	err := collection.FindOne(context.TODO(), filter).Decode(&storedUser)
+	if err != nil {
+		return false
+	}
+	return CheckPasswordHash(user.Password, storedUser.PasswordHash)
+}
+
+func HashPass(passwordhash string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(passwordhash), 14)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(passwordhash, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(passwordhash))
+	return err == nil
+}
