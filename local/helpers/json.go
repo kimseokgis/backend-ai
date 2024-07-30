8e9ab543f7e051b27770bbb421201c8ba29226f2
Farhan Rizki <farhanrizki101010@gmail.com>
2024-07-31 04:00:41 +0700

    add func encode json

--- local/helpers/json.go
+++ local/helpers/json.go
@@ -16,3 +16,10 @@ func setJSONHeader(w http.ResponseWriter) {
 func writeStatus(w http.ResponseWriter, status int) {
 	w.WriteHeader(status)
 }
+
+// encodeJSON encodes the data into JSON format and writes it to the response.
+// Takes an HTTP response writer and the data to be encoded.
+// Returns an error if encoding fails.
+func encodeJSON(w http.ResponseWriter, data interface{}) error {
+	return json.NewEncoder(w).Encode(data)
+}
