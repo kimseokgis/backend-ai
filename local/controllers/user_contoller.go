035ffe0cedb6301bfe9c0faaf23bdae9c0af7c77
Farhan Rizki <farhanrizki101010@gmail.com>
2024-07-31 03:41:28 +0700

    delete register

--- local/controllers/user_contoller.go
+++ local/controllers/user_contoller.go
@@ -12,35 +12,7 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-func RegisterUser(c *fiber.Ctx) error {
-	var user model.User
-	if err := c.BodyParser(&user); err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"message": "Invalid request payload",
-		})
-	}
 
-	// Hash password
-	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
-	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"message": "Error hashing password",
-		})
-	}
-
-	// Set hashed password
-	user.PasswordHash = string(hash)
-	user.Password = "" // Clear plain password
-
-	// Save user to database
-	conn := helper.SetConnection()
-	defer conn.Client().Disconnect(context.TODO())
-	helpers.InsertUser(conn, user)
-
-	return c.Status(fiber.StatusOK).JSON(fiber.Map{
-		"message": "User registered successfully",
-	})
-}
 
 func LoginUser(c *fiber.Ctx) error {
 	var credentials model.User
