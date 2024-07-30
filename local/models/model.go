3beb17c477780c0ddb1ce5e10a3a752e9bada891
Farhan Rizki <farhanrizki101010@gmail.com>
2024-07-31 04:25:03 +0700

    update struct

--- local/models/model.go
+++ local/models/model.go
@@ -2,12 +2,12 @@ package models
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
-// User represents a user in the system.
 type User struct {
-	ID           primitive.ObjectID `bson:"_id,omitempty"`  // ID is the unique identifier for the user.
-	Username     string             `json:"username"`       // Username is the user's username.
-	Email        string             `json:"email"`          // Email is the user's email address.
-	Password     string             `json:"password"`       // Password is the user's plain text password. This field should not be stored in the database.
-	PasswordHash string             `json:"password_hash"`  // PasswordHash is the hashed version of the user's password.
+	ID           primitive.ObjectID `bson:"_id,omitempty"`
+	Username     string             `json:"username"`
+	Email        string             `json:"email"`
+	Password     string             `json:"password"`
+	PasswordHash string             `json:"password_hash"`
 }
+
 
