4faaef076f46f2cefca17950a9dcc943f2703195
Farhan Rizki <farhanrizki101010@gmail.com>
2024-07-31 04:17:16 +0700

    delete comment

--- local/controllers/home_controller.go
+++ local/controllers/home_controller.go
@@ -6,10 +6,3 @@ import (
 	"github.com/kimseokgis/backend-ai/helper"
 )
 
-// createHomeResponse creates the response for the Home handler.
-
-
-// writeJSONResponse writes a JSON response using the helper package.
-
-
-// Home handles the home route.
