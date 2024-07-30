088cbe0d141baf8f9bc093afbb94ab942ab91c90
Farhan Rizki <farhanrizki101010@gmail.com>
2024-07-31 03:37:35 +0700

    add allowed origin

--- local/config/local_config.go
+++ local/config/local_config.go
@@ -42,6 +42,19 @@ func GetCorsConfig() cors.Config {
 	}
 }
 
+// IsAllowedOrigin checks if the origin is allowed
+func IsAllowedOrigin(origin string) bool {
+	origins := []string{
+		"http://127.0.0.1:5500",
+		"http://127.0.0.1:5501",
+	}
+	for _, o := range origins {
+		if o == origin {
+			return true
+		}
+	}
+	return false
+}
 
 var origins = []string{
 	"http://127.0.0.1:5500",
