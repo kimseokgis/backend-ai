350a1cba6c6939b0f5f66d23ae5e9ff6f7a0e2ff
Farhan Rizki <farhanrizki101010@gmail.com>
2024-07-31 03:49:39 +0700

    add create paseto token

--- local/helpers/auth.go
+++ local/helpers/auth.go
@@ -6,4 +6,13 @@ import (
 	"aidanwoods.dev/go-paseto"
 )
 
-
+// createPasetoToken initializes a new PASETO token with the given username.
+// Sets the issued at, not before, and expiration times.
+func createPasetoToken(username string) *paseto.Token {
+	token := paseto.NewToken()
+	token.SetIssuedAt(time.Now())
+	token.SetNotBefore(time.Now())
+	token.SetExpiration(time.Now().Add(2 * time.Hour))
+	token.SetString("user", username)
+	return token
+}
