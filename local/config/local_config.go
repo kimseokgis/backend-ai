8c95b62887a8f6c75f498b52ca20be5016ff3aa5
Farhan Rizki <farhanrizki101010@gmail.com>
2024-07-31 04:09:51 +0700

    delete func get cors

--- local/config/local_config.go
+++ local/config/local_config.go
@@ -12,19 +12,7 @@ import (
 
 
 
-func GetCorsConfig() cors.Config {
-	origins := []string{
-		"http://127.0.0.1:5500",
-		"http://127.0.0.1:5501",
-	}
-	return cors.Config{
-		AllowOrigins:     strings.Join(origins, ","),
-		AllowMethods:     "GET,HEAD,OPTIONS,POST,PUT",
-		AllowHeaders:     "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Request-Headers, token, Access-Control-Allow-Origin, Authorization, Bearer, login",
-		ExposeHeaders:    "Content-Length",
-		AllowCredentials: true,
-	}
-}
+
 
 // IsAllowedOrigin checks if the origin is allowed
 func IsAllowedOrigin(origin string) bool {
