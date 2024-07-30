df7d636a24fd723793631450ffbf0e1cadbf4759
Farhan Rizki <farhanrizki101010@gmail.com>
2024-07-31 04:17:39 +0700

    add new func home

--- local/controllers/home_controller.go
+++ local/controllers/home_controller.go
@@ -6,3 +6,9 @@ import (
 	"github.com/kimseokgis/backend-ai/helper"
 )
 
+func Home(w http.ResponseWriter, r *http.Request) {
+	response := map[string]string{
+		"message": "Welcome to Makmur AI!",
+	}
+	helper.WriteJSON(w, http.StatusOK, response)
+}
