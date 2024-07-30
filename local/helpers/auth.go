c237704ba15a63a59d472d9f99ae7e804987c3b0
Farhan Rizki <farhanrizki101010@gmail.com>
2024-07-31 03:49:14 +0700

    delete generate token

--- local/helpers/auth.go
+++ local/helpers/auth.go
@@ -6,15 +6,4 @@ import (
 	"aidanwoods.dev/go-paseto"
 )
 
-func GenerateToken(username, privatekey string) (string, error) {
-	token := paseto.NewToken()
-	token.SetIssuedAt(time.Now())
-	token.SetNotBefore(time.Now())
-	token.SetExpiration(time.Now().Add(2 * time.Hour))
-	token.SetString("user", username)
-	key, err := paseto.NewV4AsymmetricSecretKeyFromHex(privatekey)
-	if err != nil {
-		return "", err
-	}
-	return token.V4Sign(key, nil), nil
-}
+
