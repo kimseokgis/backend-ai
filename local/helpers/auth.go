0a74e9a7f48e5796774d5e21ab9754dcf28393e1
Farhan Rizki <farhanrizki101010@gmail.com>
2024-07-31 04:18:12 +0700

    delete create paseto token

--- local/helpers/auth.go
+++ local/helpers/auth.go
@@ -7,15 +7,7 @@ import (
 )
 
 // createPasetoToken initializes a new PASETO token with the given username.
-// Sets the issued at, not before, and expiration times.
-func createPasetoToken(username string) *paseto.Token {
-	token := paseto.NewToken()
-	token.SetIssuedAt(time.Now())
-	token.SetNotBefore(time.Now())
-	token.SetExpiration(time.Now().Add(2 * time.Hour))
-	token.SetString("user", username)
-	return token
-}
+
 
 // getPasetoSecretKey generates a PASETO secret key from a given hex string.
 // Returns the secret key or an error if key generation fails.
