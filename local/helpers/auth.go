8bc90b5d1301ef50b7cf60f43e7c9286e58f600d
Farhan Rizki <farhanrizki101010@gmail.com>
2024-07-31 03:50:50 +0700

    add new generate token

--- local/helpers/auth.go
+++ local/helpers/auth.go
@@ -32,3 +32,14 @@ func getPasetoSecretKey(privatekey string) (*paseto.V4AsymmetricSecretKey, error
 func signPasetoToken(token *paseto.Token, key *paseto.V4AsymmetricSecretKey) string {
 	return token.V4Sign(key, nil)
 }
+
+// GenerateToken generates a PASETO token for the given username and private key.
+// Returns the signed token or an error if any step fails.
+func GenerateToken(username, privatekey string) (string, error) {
+	token := createPasetoToken(username)
+	key, err := getPasetoSecretKey(privatekey)
+	if err != nil {
+		return "", err
+	}
+	return signPasetoToken(token, key), nil
+}
