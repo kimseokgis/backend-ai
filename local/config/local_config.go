c7762f0a4fb87de46968542dff0605b53ee0108a
Farhan Rizki <farhanrizki101010@gmail.com>
2024-07-31 04:10:03 +0700

    delete func origin

--- local/config/local_config.go
+++ local/config/local_config.go
@@ -14,16 +14,4 @@ import (
 
 
 
-// IsAllowedOrigin checks if the origin is allowed
-func IsAllowedOrigin(origin string) bool {
-	origins := []string{
-		"http://127.0.0.1:5500",
-		"http://127.0.0.1:5501",
-	}
-	for _, o := range origins {
-		if o == origin {
-			return true
-		}
-	}
-	return false
-}
+
