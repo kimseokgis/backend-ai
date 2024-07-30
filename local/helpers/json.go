f344f541b29cebaff4f5e4a0aea818b76e028e2d
Farhan Rizki <farhanrizki101010@gmail.com>
2024-07-31 03:59:39 +0700

    delete write json

--- local/helpers/json.go
+++ local/helpers/json.go
@@ -5,8 +5,4 @@ import (
 	"net/http"
 )
 
-func WriteJSON(w http.ResponseWriter, status int, data interface{}) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(status)
-	json.NewEncoder(w).Encode(data)
-}
+
