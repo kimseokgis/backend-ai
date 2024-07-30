8c36bbc16ac689ffaa395c6121db95a5e441a1b3
harisriyoni3 <1214047@std.ulbi.ac.id>
2024-07-30 07:26:42 +0700

    add fiber and middleware

--- local/main.go
+++ local/main.go
@@ -1,1 +1,14 @@
 package local
+
+import (
+	"github.com/gofiber/fiber"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/kimseokgis/backend-ai/local/config"
+)
+
+func main() {
+	app := fiber.New(config.FiberConfig)
+
+	// Middleware
+	app.Use(logger.New())
+}
