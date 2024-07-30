1ffb572221b3bc7faf8513e4e3717deb9115f75e
Farhan Rizki <farhanrizki101010@gmail.com>
2024-07-31 04:00:24 +0700

    add func write status

--- local/helpers/json.go
+++ local/helpers/json.go
@@ -10,3 +10,9 @@ import (
 func setJSONHeader(w http.ResponseWriter) {
 	w.Header().Set("Content-Type", "application/json")
 }
+
+// writeStatus sets the HTTP status code for the response.
+// Takes an HTTP response writer and the status code.
+func writeStatus(w http.ResponseWriter, status int) {
+	w.WriteHeader(status)
+}
