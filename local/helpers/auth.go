085857165cc9eaa8503fe5058fbf71b3c21ced96
Farhan Rizki <farhanrizki101010@gmail.com>
2024-07-31 04:18:27 +0700

    delete get paseto secret key

--- local/helpers/auth.go
+++ local/helpers/auth.go
@@ -10,14 +10,7 @@ import (
 
 
 // getPasetoSecretKey generates a PASETO secret key from a given hex string.
-// Returns the secret key or an error if key generation fails.
-func getPasetoSecretKey(privatekey string) (*paseto.V4AsymmetricSecretKey, error) {
-	key, err := paseto.NewV4AsymmetricSecretKeyFromHex(privatekey)
-	if err != nil {
-		return nil, err
-	}
-	return key, nil
-}
+
 
 // signPasetoToken signs the token with the given secret key.
 // Returns the signed token as a string.
