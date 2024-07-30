2e635239640a9e93d7494e4a05b731c3757228cd
Farhan Rizki <farhanrizki101010@gmail.com>
2024-07-31 04:19:04 +0700

    delete comment

--- local/helpers/auth.go
+++ local/helpers/auth.go
@@ -6,13 +6,3 @@ import (
 	"aidanwoods.dev/go-paseto"
 )
 
-// createPasetoToken initializes a new PASETO token with the given username.
-
-
-// getPasetoSecretKey generates a PASETO secret key from a given hex string.
-
-
-// signPasetoToken signs the token with the given secret key.
-
-
-// GenerateToken generates a PASETO token for the given username and private key.
