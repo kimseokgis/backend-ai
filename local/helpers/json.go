e9f9091b8feed3b59d29fd592a98dc11a560d92a
Farhan Rizki <farhanrizki101010@gmail.com>
2024-07-31 04:00:06 +0700

    add func set json header

--- local/helpers/json.go
+++ local/helpers/json.go
@@ -5,4 +5,8 @@ import (
 	"net/http"
 )
 
-
+// setJSONHeader sets the Content-Type header to application/json.
+// Takes an HTTP response writer.
+func setJSONHeader(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "application/json")
+}
