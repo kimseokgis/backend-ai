6acf26014f460f7a6a906c4682a15687fb93f46f
Daffa Audya P <daffaaudyapramana03@gmail.com>
2024-08-01 01:42:37 +0700

    revert main.go

--- main.go
+++ main.go
@@ -1,24 +1,10 @@
 package backend_ai
 
 import (
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors"
-	"github.com/kimseokgis/backend-ai/config"
-	"github.com/kimseokgis/backend-ai/routes"
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+	"github.com/kimseokgis/backend-ai/url"
 )
 
-func main() {
-	app := fiber.New()
-
-	// Connect to the database
-	config.ConnectDatabase()
-
-	// Middleware
-	app.Use(cors.New())
-
-		// Setup routes
-		routes.SetupRoutes(app)
-
-		// Start the server
-		app.Listen(":8080")
+func init() {
+	functions.HTTP("makmur", url.Web)
 }
