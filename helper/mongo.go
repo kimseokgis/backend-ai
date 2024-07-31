86075adb36cdc6f1ad6d8af4b8f89144089e536d
rofinafiin <rofinafiisr@gmail.com>
2024-07-31 23:11:57 +0700

    reverting commit sha 47052d26d584accda23d88654e4c3925ac70ab67

--- helper/mongo.go
+++ helper/mongo.go
@@ -229,16 +229,24 @@ func Stemmer(Sentences string) (newString string) {
 	dictionary := sastrawi.DefaultDictionary()
 	stemmer := sastrawi.NewStemmer(dictionary)
 	for _, word := range sastrawi.Tokenize(Sentences) {
+		//fmt.Println(word)
 		newString = newString + " " + stemmer.Stem(word)
+		//fmt.Println(newString)
 	}
 	return strings.TrimSpace(newString)
 }
 
 // Fungsi untuk memisahkan kata dengan imbuhan "mu" di akhir
 func SeparateSuffixMu(word string) string {
+	// Regex untuk mendeteksi kata dengan imbuhan "mu" di akhir
 	re := regexp.MustCompile(`(\w+)(mu)$`)
+
+	// Cek apakah kata cocok dengan regex
 	if re.MatchString(word) {
+		// Ganti "mu" dengan " kamu"
 		return re.ReplaceAllString(word, "$1 kamu")
 	}
+
+	// Jika tidak ada imbuhan "mu", kembalikan kata asli
 	return word
 }
