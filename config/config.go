100574076efb1749701dc2d449b563daf5c56d49
Daffa Audya P <daffaaudyapramana03@gmail.com>
2024-08-01 00:18:20 +0700

    update config.go

--- config/config.go
+++ config/config.go
@@ -9,14 +9,15 @@ import (
 var Iteung = fiber.Config{
 	Prefork:       true,
 	CaseSensitive: true,
-	StrictRouting: true,
-	ServerHeader:  "Makmur",
-	AppName:       "Makmur ai",
 
+	func ConnectDatabase() {
+		dsn := os.Getenv("DB_USER") + ":" + os.Getenv("DB_PASS") + "@tcp(" + os.Getenv("DB_HOST") + ")/" + os.Getenv("DB_NAME") + "?charset=utf8mb4&parseTime=True&loc=Local"
+		database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		
 	if err != nil {
 		panic("Failed to connect to the database!")
 	}
-	
+
 DB = database
 fmt.Println("Database connection established")
 }
