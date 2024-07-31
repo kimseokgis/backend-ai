bee4fc758513a2a13f437c2c9cd1e5a7b0065c8e
Daffa Audya P <daffaaudyapramana03@gmail.com>
2024-08-01 01:38:43 +0700

    refactor func main.go

--- run/main.go
+++ run/main.go
@@ -29,5 +29,9 @@ func main() {
 	loggedMux := loggingMiddleware(mux)
 
 	// Start the server
-	app.Listen(":8080")
+	log.Println("Starting server on :8080")
+	err := http.ListenAndServe(":8080", loggedMux)
+	if err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
 }
