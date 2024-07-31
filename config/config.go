227b7446513881e21e5ca4d99097dfeeada98a55
Daffa Audya P <daffaaudyapramana03@gmail.com>
2024-08-01 00:39:20 +0700

    refactor config.go

--- config/config.go
+++ config/config.go
@@ -10,10 +10,9 @@ import (
 var DB *gorm.DB
 
 // ConnectDatabase initializes the database connection
-	func ConnectDatabase() {
-		dsn := os.Getenv("DB_USER") + ":" + os.Getenv("DB_PASS") + "@tcp(" + os.Getenv("DB_HOST") + ")/" + os.Getenv("DB_NAME") + "?charset=utf8mb4&parseTime=True&loc=Local"
-		database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
+				ServerHeader:  "Makmur",
+				AppName:       "Makmur ai",
+}
 		var IPport, netstring = helper.GetAddress()
 
 	var PrivateKey = os.Getenv("PRIVATEKEY")
