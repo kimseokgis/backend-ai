e280d4bb362d8160b55a5718fd7bdbf3609c7167
Daffa Audya P <daffaaudyapramana03@gmail.com>
2024-07-31 20:48:09 +0700

    update import

--- local/helpers/db.go
+++ local/helpers/db.go
@@ -3,10 +3,3 @@ package helpers
 import (
 	"context"
 	"os"
-
-	"github.com/aiteung/atdb"
-	"github.com/kimseokgis/backend-ai/model"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"golang.org/x/crypto/bcrypt"
-)
