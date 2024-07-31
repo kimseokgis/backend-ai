3beb0ee1dce953133b6f11730d2e905e93d3fdb6
Daffa Audya P <daffaaudyapramana03@gmail.com>
2024-07-31 20:01:34 +0700

    delete struct

--- local/models/model.go
+++ local/models/model.go
@@ -1,13 +1,3 @@
 package models
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
-
-type User struct {
-	ID           primitive.ObjectID `bson:"_id,omitempty"`
-	Username     string             `json:"username"`
-	Email        string             `json:"email"`
-	Password     string             `json:"password"`
-	PasswordHash string             `json:"password_hash"`
-}
-
-
