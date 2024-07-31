693b422af20c032b9b3d1864493df4614c4f562d
Daffa Audya P <daffaaudyapramana03@gmail.com>
2024-07-31 20:49:22 +0700

    delete func generatetoken

--- local/helpers/auth.go
+++ local/helpers/auth.go
@@ -5,9 +5,3 @@ import (
 
 	"aidanwoods.dev/go-paseto"
 )
-
-func GenerateToken(username, privatekey string) (string, error) {
-	token := paseto.NewToken()
-	token.SetIssuedAt(time.Now())
-	token.SetNotBefore(time.Now())
-	token.SetExpiration(time.Now().Add(2 * time.Hour))
