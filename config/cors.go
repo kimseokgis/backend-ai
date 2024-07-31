89b2823a46c10bcc628ddae08187515d8b1ced52
Daffa Audya P <daffaaudyapramana03@gmail.com>
2024-08-01 00:48:12 +0700

    update cors.go

--- config/cors.go
+++ config/cors.go
@@ -20,46 +20,7 @@ func GenerateToken(userID string) (string, error) {
 
 // ErrorResponse sends an error response
 func ErrorResponse(c *fiber.Ctx, message string) error {
-}
-
-var Internalhost string = os.Getenv("INTERNALHOST") + ":" + os.Getenv("PORT")
-
-var Cors = cors.Config{
-	AllowOrigins:     strings.Join(origins[:], ","),
-	AllowMethods:     "GET,HEAD,OPTIONS,POST,PUT",
-	AllowHeaders:     "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Request-Headers, token, Access-Control-Allow-Origin, Authorization, Bearer, login",
-	ExposeHeaders:    "Content-Length",
-	AllowCredentials: true,
-}
-
-func isAllowedOrigin(origin string) bool {
-	for _, o := range origins {
-		if o == origin {
-			return true
-		}
-	}
-	return false
-}
-
-func SetAccessControlHeaders(w http.ResponseWriter, r *http.Request) bool {
-	origin := r.Header.Get("Origin")
-
-	if isAllowedOrigin(origin) {
-		// Set CORS headers for the preflight request
-		if r.Method == http.MethodOptions {
-			w.Header().Set("Access-Control-Allow-Credentials", "true")
-			w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Request-Headers, token, Access-Control-Allow-Origin, Authorization, Bearer, login")
-			w.Header().Set("Access-Control-Allow-Methods", "POST,GET,DELETE,PUT")
-			w.Header().Set("Access-Control-Allow-Origin", origin)
-			w.Header().Set("Access-Control-Max-Age", "3600")
-			w.WriteHeader(http.StatusNoContent)
-			return true
-		}
-		// Set CORS headers for the main request.
-		w.Header().Set("Access-Control-Allow-Credentials", "true")
-		w.Header().Set("Access-Control-Allow-Origin", origin)
-		return false
-	}
-
-	return false
+	return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+		"error": message,
+	})
 }
