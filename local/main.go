64d36f7c44724b23582ed666467bcf7880ff14e0
Daffa Audya P <daffaaudyapramana03@gmail.com>
2024-07-31 19:50:14 +0700

    update import

--- local/main.go
+++ local/main.go
@@ -3,9 +3,3 @@ package main
 import (
 	"log"
 	"os"
-
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/logger"
-	"github.com/kimseokgis/backend-ai/local/config"
-	"github.com/kimseokgis/backend-ai/local/controllers"
-)
